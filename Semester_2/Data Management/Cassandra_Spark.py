33f47131fe84eef16fcffe4966896ea3ec8c0a74
Ahmad Hathim bin Ahmad Azman <88342227+hathimazman@users.noreply.github.com>
2025-05-29 09:50:40 +0800

    class

--- Semester_2/Data Management/Cassandra_Spark.py
+++ Semester_2/Data Management/Cassandra_Spark.py
@@ -25,3 +25,46 @@ spark = SparkSession.builder \
 # Test connection
 print(f"Spark Version: {spark.version}")
 print("Cassandra connector loaded successfully!")
+
+
+
+from pyspark.sql import SparkSession
+from pyspark.sql import Row
+from pyspark.sql import functions
+
+def parseInput(line):
+    fields = line.split('|')
+    return Row(user_id=int(fields[0]), age=int(fields[1]), gender=fields[2], occupation=fields[3], zip=fields[4])
+
+if __name__ == "__main__":
+   # Create SparkSession
+   spark = Spark.session.builder \
+        .appName("CassandraIntegration") \
+        .config("spark.cassandra.connection.host", "127.0.0.1") \
+        .getOrCreate()
+   
+   # Get the raw data
+   lines = spark.SparkContext.textFile("hdfs:///user/maria_dev/hathim/ml-100k/u.user")
+   
+   # conver to RDD of Row objects
+   usersDataset = lines.map(parseInput)
+
+   # Convert to DataFrame
+   usersDataset.write \
+        .format("org.apache.spark.sql.cassandra") \
+        .mode("append") \
+        .options(table="users", keyspace="movielens") \
+        .save()
+   
+   # Read it back from Cassandra into new dataframe
+   readUsers = spark.read \
+        .format("org.apache.spark.sql.cassandra") \
+        .options(table="users", keyspace="movielens") \
+        .load()
+   
+   readUsers.createOrReplaceTempView("users")
+
+   sqlDF = spark.sql("SELECT * FROM users WHERE age > 20")
+   sqlDF.show()
+
+   spark.stop()
